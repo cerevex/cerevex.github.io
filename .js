ed8405f27647eda49559b5ac1dd52cec4c851f44
cerevex <cerevex69@gmail.com>
2025-04-20 21:49:41 +0530

    .js

---
+++ .js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCQlPHCEBxzezcgQ0pNNLhLRYhb0VC37Ag",
+    authDomain: "cerevex-6b6a6.firebaseapp.com",
+    projectId: "cerevex-6b6a6",
+    storageBucket: "cerevex-6b6a6.firebasestorage.app",
+    messagingSenderId: "532463948818",
+    appId: "1:532463948818:web:246769054faf0bd8f634cb"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
